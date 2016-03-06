cc36b7d56af4e572c502d775ca14a9074dc585a8
Tim Dang <timothy.dang@gmail.com>
2016-03-05 20:40:10 -0800

    chat refresh

--- Mobile_Client/www/js/chat/chat.js
+++ Mobile_Client/www/js/chat/chat.js
@@ -3,14 +3,20 @@ angular.module('hackoverflow.chat', [
     'ui.router'
   ])
 
-.controller('ChatController', function($scope, socket, $state) {
+.controller('ChatController', function($scope, socket) {
   $scope.msgs = [];
+  $scope.text = '';
+
   $scope.sendMsg = function(msg) {
+    // greatest line of code ever written
+    $scope.$$childTail.text = null;
     // sends message to server
     socket.emit('send msg', msg);
   };
   // client receives message from server
   socket.on('get msg', function(msg) {
     $scope.msgs.push(msg);
+
+    $scope.$digest();
   });
 });
