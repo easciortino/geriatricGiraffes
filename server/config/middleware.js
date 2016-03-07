e8c2aa87bd13c71356d4b81f5f1c19f910a7b031
Tim Dang <timothy.dang@gmail.com>
2016-03-07 11:22:09 -0800

    checking remote origin

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -126,7 +126,7 @@ module.exports = function(app, express) {
     var accessTokenUrl = 'https://github.com/login/oauth/access_token';
     var userApiUrl = 'https://api.github.com/user';
     var md = new MobileDetect(req.headers['user-agent']);
-    console.log('\n###MobileDetect\n',md);
+    console.log('\n###MobileDetect\n',md.maxPhoneWidth);
     var params = {
       code: req.body.code,
       client_id: req.body.clientId,
